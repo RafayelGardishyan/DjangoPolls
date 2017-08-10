a4a97bbd558f7b732ba30c24352f1cde3ae4c9bd
RafayelGardishyan <rgardishyan@gmail.com>
2017-08-10 14:44:38 +0200

    Added redirect to polls app from the root directory

--- FirstSite/urls.py
+++ FirstSite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
+    url(r'^$', lambda r: HttpResponseRedirect('polls/')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
